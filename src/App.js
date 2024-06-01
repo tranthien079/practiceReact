5a5a71f56ff4378fad218b33d6ff58eea4244c27
iuh20053901 <tranminhthien0709@gmail.com>
2024-06-01 13:45:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const courses = [
+  {
+    id: 1,
+    name: "React",
+  },
+  {
+    id: 2,
+    name: "Vue",
+  },
+  {
+    id: 3,
+    name: "Angular",
+  },
+];
 
 function App() {
+  const [checked, setChecked] = useState([]);
+  console.log(checked);
+
+  const handleChecked = (id) => {
+    const isChecked = checked.includes(id);
+    setChecked(prev => {
+      if(isChecked) {
+        return checked.filter(item  => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+  }
+
+  const handleSubmit = () => {
+    console.log({id: checked});
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      {courses.map(course => (
+        <div key={course.id}>
+          <input type="checkbox" checked={checked.includes(course.id)}  onChange={() => handleChecked(course.id)}/> {course.name}
+        </div>
+      ))}
+   
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
